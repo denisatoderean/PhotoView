737d37c630aef8bcbf41d003d9dae0dc1d185e0c
Denisa Toderean <denisa.toderean@gmail.com>
2014-07-23 11:58:18 +0300

    - added new base class

---
+++ PhotoViewer/PhotoViewer/Classes/DTViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTViewController.h
+//  PhotoViewer
+//
+//  Created by Denisa Toderean on 23/07/14.
+//  Copyright (c) 2014 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTViewController : UIViewController
+
+@end
