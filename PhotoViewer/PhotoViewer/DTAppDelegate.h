c9588156347bf1939e92e8aaaddcd5909ecb70af
Denisa Toderean <Denisa@Cynnys-Mac-mini.local>
2014-07-23 11:52:07 +0300

    - created project

---
+++ PhotoViewer/PhotoViewer/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  PhotoViewer
+//
+//  Created by Denisa Toderean on 23/07/14.
+//  Copyright (c) 2014 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoViewer/PhotoViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoViewer
+//
+//  Created by Denisa Toderean on 23/07/14.
+//  Copyright (c) 2014 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
